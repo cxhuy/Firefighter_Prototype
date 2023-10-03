845011e35f8be4fa1adf22516370c0536b4eb15a
cxhuy <alvinkang1@gmail.com>
2023-10-03 01:33:21 -0500

    feat: change hose segment size

--- Scenes/hose.gd
+++ Scenes/hose.gd
@@ -1,24 +1,25 @@
 extends Node2D
 
-var rope_segment = preload("res://Scenes/hose_segment.tscn")
-var rope_length = 10
+var hose_segment = preload("res://Scenes/hose_segment.tscn")
+var hose_length = 30
+var hose_segment_length = 10
 
 func _ready():
 	var previous_segment = null
-	for i in range(rope_length):
-		var segment_instance = rope_segment.instantiate()
-		segment_instance.position = Vector2(0, 30 * i)
+	for i in range(hose_length):
+		var segment_instance = hose_segment.instantiate()
+		segment_instance.position = Vector2(0, hose_segment_length * i)
 		add_child(segment_instance)
 		if previous_segment != null:
 			var joint = PinJoint2D.new()
-			joint.position = Vector2(0, 30 * (i - 1) + 15)
+			joint.position = Vector2(0, hose_segment_length * (i - 1) + hose_segment_length / 2)
 			joint.node_a = previous_segment.get_path()
 			joint.node_b = segment_instance.get_path()
 			add_child(joint)
 		previous_segment = segment_instance
 		if i == 0:
 			var mainJoint = PinJoint2D.new()
-			mainJoint.position = Vector2(0, -15)
+			mainJoint.position = Vector2(0, -(hose_segment_length / 2))
 			mainJoint.node_a = get_node("../StaticBody2D").get_path()
 			mainJoint.node_b = previous_segment.get_path()
 			add_child(mainJoint)
