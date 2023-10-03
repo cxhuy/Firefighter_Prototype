6cea0bcb55576c7acf6b5febc896abaa8a325324
cxhuy <alvinkang1@gmail.com>
2023-10-02 21:11:52 -0500

    feat: add character sprite flip

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 const speed = 100
 
+@onready var anim = get_node("AnimatedSprite2D")
+
 func _physics_process(delta):
 	character_movement(delta)
 	
 func character_movement(delta):
 	if Input.is_action_pressed("ui_right"):
+		anim.flip_h = false
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		anim.flip_h = true
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
